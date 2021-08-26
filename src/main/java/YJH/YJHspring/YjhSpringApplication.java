a3465396ce5a812be6073ba1260dbffe9624646e
YANHJIHUN <ik5334@naver.com>
2021-08-26 10:47:06 +0900

    김영한 MVC 스프링 재작업

---
+++ src/main/java/YJH/YJHspring/YjhSpringApplication.java
@@ -0,0 +1,13 @@
+package YJH.YJHspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YjhSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YjhSpringApplication.class, args);
+	}
+
+}
---
+++ src/main/java/YJH/YJHspring/controller/YjhController.java
@@ -0,0 +1,15 @@
+package YJH.YJHspring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class YjhController {
+
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "hello!!");
+        return  "hello";
+    }
+}
