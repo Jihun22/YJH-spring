191631e7af46bc8474efb1cbc3e9e7067d723b81
YANHJIHUN <ik5334@naver.com>
2021-08-27 14:09:15 +0900

    회원서비스 개발

--- src/main/java/YJH/YJHspring/service/MemberService.java
+++ src/main/java/YJH/YJHspring/service/MemberService.java
@@ -4,6 +4,7 @@ import YJH.YJHspring.domain.Member;
 import YJH.YJHspring.repository.MemberRepository;
 import YJH.YJHspring.repository.MemoryMemberRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public class MemberService {
@@ -28,5 +29,16 @@ public class MemberService {
             .ifPresent(m ->{
             throw new IllegalStateException("이미 존재하는 회원입니다.");
         });
+    }
+
+    /**
+    전체 회원 조회
+     */
+    public List <Member> findMembers() {
+       return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
     }
 }
