0eb5a8aebb66db59212f6393c12008421b6b607f
YANHJIHUN <ik5334@naver.com>
2021-09-02 23:52:11 +0900

    회원목록

--- src/main/java/YJH/YJHspring/controller/MemberController.java
+++ src/main/java/YJH/YJHspring/controller/MemberController.java
@@ -4,8 +4,11 @@ import YJH.YJHspring.domain.Member;
 import YJH.YJHspring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -30,4 +33,11 @@ public class MemberController {
 
         return  "redirect:/";
     }
+    @GetMapping ("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members",members);
+        return  "members/memberList";
+    }
+
 }
