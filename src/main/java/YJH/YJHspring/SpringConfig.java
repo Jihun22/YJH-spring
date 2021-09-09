514bc895e2799c90fe4fcadf3cb81835ed8c49de
YANHJIHUN <ik5334@naver.com>
2021-09-09 23:46:32 +0900

    db세팅

--- src/main/java/YJH/YJHspring/SpringConfig.java
+++ src/main/java/YJH/YJHspring/SpringConfig.java
@@ -1,13 +1,23 @@
 package YJH.YJHspring;
 
+import YJH.YJHspring.repository.JdbcTemplateMemberRepository;
 import YJH.YJHspring.repository.MemberRepository;
 import YJH.YJHspring.repository.MemoryMemberRepository;
 import YJH.YJHspring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+    private DataSource dataSource;
+
+    @Autowired
+    public  SpringConfig(DataSource dataSource){
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService () {
@@ -17,6 +27,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository () {
-        return  new MemoryMemberRepository();
+//        return  new MemoryMemberRepository();
+        return  new JdbcTemplateMemberRepository(dataSource);
     }
 }
