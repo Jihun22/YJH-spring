9c7a0e2551cfe380a1bead24b4d23a6c529249e1
YANHJIHUN <ik5334@naver.com>
2021-09-27 22:56:15 +0900

    테스트

--- src/test/java/YJH/YJHspring/service/MemberServiceIntegrationTest.java
+++ src/test/java/YJH/YJHspring/service/MemberServiceIntegrationTest.java
@@ -37,20 +37,16 @@ class MemberServiceIntegrationTest {
 
     }
     @Test
-    public  void 중복_회원_예외() {
-        //given
+    public void 중복_회원_예외() throws Exception {
+        //Given
         Member member1 = new Member();
         member1.setName("spring");
-
         Member member2 = new Member();
         member2.setName("spring");
-        //when
+        //When
         memberService.join(member1);
         IllegalStateException e = assertThrows(IllegalStateException.class,
                 () -> memberService.join(member2));//예외가 발생해야 한다.
-
         assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
     }
-
-
-    }
+}
