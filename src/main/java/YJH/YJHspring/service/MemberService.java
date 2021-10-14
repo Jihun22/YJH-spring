5a855796b061a7fb87c9b4bc8b465766ad03f880
YANHJIHUN <ik5334@naver.com>
2021-10-14 18:18:00 +0900

    Aoa 3:08 부분

--- src/main/java/YJH/YJHspring/service/MemberService.java
+++ src/main/java/YJH/YJHspring/service/MemberService.java
@@ -24,11 +24,21 @@ public class MemberService {
      회원가입
      */
     public  Long join (Member member) {
+
+        long start =System.currentTimeMillis();
+
+        try {
+            validateDuplicateMember(member); //중복회원 검증
+            memberRepository.save(member);
+            return member.getId();
+        }finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join= " +timeMs+ "ms");
+        }
         //같은 이름이 있는 중복 회원 X
        // Optional<Member> result = memberRepository.findByName(member.getName()); //널이 아니라 값이 있으면 로직 동작
-        validateDuplicateMember(member); //중복회원 검증
-        memberRepository.save(member);
-        return member.getId();
+
 
     }
 
